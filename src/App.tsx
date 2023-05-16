2ce09fdd42ae8119736f6b70ba49522cbdeac5b3
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-16 14:00:08 +0300

    initializing project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/header'
+import Footer from './components/footer'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
