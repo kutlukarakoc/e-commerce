81d1dd29ce880408e55d3c1948a94eadf8b5ae38
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-19 00:17:05 +0300

    footer padding improvements

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -7,7 +7,7 @@ import { items } from '../../constants/footer/footerConstants'
 const Footer: React.FC = () => {
    return (
       <footer className="min-h-72 flex flex-col justify-evenly gap-4 sm:gap-10 py-4 border-t border-solid border-gray-500">
-         <div className="flex flex-col sm:flex-row gap-4 justify-around items-center px-2">
+         <div className="flex flex-col sm:flex-row gap-4 justify-around items-center px-2 pt-6 pb-2">
             <div className='w-32 h-32 sm:w-40 sm:h-40 flex items-center'>
                <img src={logo} alt="ecommerce" className='w-full block' />
             </div>
