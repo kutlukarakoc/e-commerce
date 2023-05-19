0bd2835eb2e4010f15e650085baebab9672e093c
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-20 00:06:45 +0300

    rating rating bugfix

--- src/components/ui/rating/index.tsx
+++ src/components/ui/rating/index.tsx
@@ -1,12 +1,11 @@
+import './style.css'
+
 interface IRating {
    rate: number
 }
 
 const Rating: React.FC<IRating> = ({ rate }) => {
-
-   const afterWidth = rate * 20
-
-   return <i data-star={rate} className={`text-left inline-block relative before:block before:content-["★★★★★"] before:text-gray-400 after:whitespace-nowrap after:absolute after:left-0 after:top-0 after:content-["★★★★★"] after:w-[${afterWidth}%] after:text-orange-400 after:overflow-hidden h-full`}></i>
+   return <i data-star={rate} className={`h-full text-left inline-block relative before:block before:content-["★★★★★"] before:text-gray-400 after:whitespace-nowrap after:absolute after:left-0 after:top-0 after:content-["★★★★★"] after:text-orange-400 after:overflow-hidden`}></i>
 }
 
 export default Rating
