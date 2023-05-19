faae03afc89a353502dab5f9af3176a956fcf382
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-19 22:57:57 +0300

    changed logic of rating component

---
+++ src/constants/product/accordionConstants.ts
@@ -0,0 +1,19 @@
+export const shippingConstants = {
+   title: 'Shipping',
+   contents: [
+      'Free shipping on orders over $100',
+      'International shipping available',
+      'Expedited shipping options',
+      'Signature required upon delivery'
+   ]
+}
+
+export const returnsConstants = {
+   title: 'Returns',
+   contents: [
+      'Easy return requests',
+      'Pre-paid shipping label included',
+      '10% restocking fee for returns',
+      '30 day return window'
+   ]
+}
