5bca0f5958c10929a7a8a596621e6c159ec8155f
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-22 01:29:32 +0300

    added products container component

---
+++ src/pages/category/products.tsx
@@ -0,0 +1,12 @@
+import { useAppSelector, useAppDispatch } from '../../store/hooks'
+
+const Products = () => {
+
+   return (
+      <div className='flex-1'>
+         products
+      </div>
+   )
+}
+
+export default Products 
