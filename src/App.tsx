2b6833fb1bd48e13c2f30d61b8e15639d7d09ef8
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-21 18:03:14 +0300

    changed indent spaces

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,24 @@ import Footer from './components/footer'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Product from './pages/product'
+import AllCategories from './pages/category'
 
 const App: React.FC = () => {
-  return (
-    <main className='font-sans h-full flex flex-col text-gray-700'>
-      <Header />
-      <div className='flex-1'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/products/:productId' element={<Product />} />
-        </Routes>
-      </div>
-      <Footer />
-    </main>
-  )
+	return (
+		<main className='font-sans h-full flex flex-col text-gray-700'>
+			<Header />
+			<div className='flex-1'>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/products/:productId' element={<Product />} />
+					<Route path='/category' element={<AllCategories />}>
+						
+					</Route>
+				</Routes>
+			</div>
+			<Footer />
+		</main>
+	)
 }
 
 export default App
