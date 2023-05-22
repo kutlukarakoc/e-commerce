a05c4128f486c470d21b29949be44e5234d585bc
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-22 22:33:57 +0300

    changed focus type

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -20,7 +20,7 @@ const Input: React.FC<InputProps> = ({ type, name, label, placeholder, wrapperSt
             id={name}
             {...rest}
             className={'peer outline-none h-full text-gray-700 w-full border border-gray-500 rounded px-2 py-1 text-sm focus:border-indigo-600 ' + inputStyles} />
-         <span className='absolute text-sm whitespace-nowrap left-0 translate-x-2 top-1/2 -translate-y-1/2 transition-all peer-focus-within:text-xs peer-focus:text-indigo-600 peer-focus:top-0 peer-focus:bg-white'>{placeholder}</span>
+         <span className='absolute text-sm whitespace-nowrap left-0 translate-x-2 top-1/2 -translate-y-1/2 transition-all peer-focus:text-xs peer-focus:text-indigo-600 peer-focus:top-0 peer-focus:bg-white'>{placeholder}</span>
       </div>
    )
 }
