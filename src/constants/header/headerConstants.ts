694538654b336003031b83c04b5900f42b554630
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-23 23:07:05 +0300

    changed title and path of women and men

--- src/constants/header/headerConstants.ts
+++ src/constants/header/headerConstants.ts
@@ -8,11 +8,11 @@ export const categories = [
       path: '/products/category/jewelery'
    },
    {
-      title: 'Men\'s Clothing',
-      path: '/products/category/men\'s clothing'
+      title: 'Men\'s clothing',
+      path: '/products/category/men'
    },
    {
-      title: 'Women\'s Clothing',
-      path: '/products/category/women\'s clothing'
+      title: 'Women\'s clothing',
+      path: '/products/category/women'
    },
 ]
