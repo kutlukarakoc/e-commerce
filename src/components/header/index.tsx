289391ce0fa7b1bcd85be70d2e14a18bd8a7d9f0
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-23 23:55:08 +0300

    bugfix

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -26,15 +26,19 @@ const Header: React.FC = () => {
    }, [toggleMenu])
 
    useEffect(() => {
-      if(pathname.includes('search-results')) {
+      if (pathname.includes('search-results')) {
          setInputValue(search.split('=')[1])
+      } else {
+         setInputValue('')
       }
+
+      return () => setInputValue('')
    }, [search])
 
    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
       event.preventDefault()
       const value = new FormData(event.currentTarget).get('head-search')
-      if(value){
+      if (value) {
          navigate(`/search-results?search=${value}`)
       }
    }
@@ -56,7 +60,7 @@ const Header: React.FC = () => {
                   <img src={logo} alt='ecommerce' className='w-full h-full block' />
                </Link>
                <form className='hidden sm:block order-2 max-w-xs w-full relative' onSubmit={handleSubmit}>
-                  <Input type='text' name='head-search' placeholder='Search products' defaultValue={inputValue} wrapperStyles='h-9' autoComplete='off' pattern='.+' required />
+                  <Input type='text' name='head-search' placeholder='Search products' value={inputValue} wrapperStyles='h-9' autoComplete='off' pattern='.+' required onChange={e => setInputValue(e.target.value)} />
                   <button type='submit' className='absolute right-3 top-1/2 -translate-y-1/2'>
                      <MagnifyingGlassIcon className='w-5 h-5' />
                   </button>
@@ -75,7 +79,7 @@ const Header: React.FC = () => {
                   <Link key={index} to={category.path} className='hidden sm:block text-gray-800'>{category.title}</Link>
                ))}
                <form className='block sm:hidden w-11/12 mx-auto relative' onSubmit={handleSubmit}>
-                  <Input type='text' name='head-search' placeholder='Search products' defaultValue={inputValue} wrapperStyles='h-9' />
+                  <Input type='text' name='head-search' placeholder='Search products' value={inputValue} wrapperStyles='h-9' autoComplete='off' pattern='.+' required onChange={e => setInputValue(e.target.value)} />
                   <button type='submit' className='absolute right-3 top-1/2 -translate-y-1/2'>
                      <MagnifyingGlassIcon className='w-5 h-5' />
                   </button>
