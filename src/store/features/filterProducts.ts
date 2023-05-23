f3471adb53d3765e297901cfbe4f8050ac95ed5b
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-23 20:44:28 +0300

    added price sort and rate sort logic

--- src/store/features/filterProducts.ts
+++ src/store/features/filterProducts.ts
@@ -6,10 +6,54 @@ import axios from 'axios'
 export const fetchProductsByUrl = createAsyncThunk(
    'sortedProducts/byKey',
    async (url: string) => {
-      const response = await axios.get(`https://fakestoreapi.com${url}`)
-      return response.data
+      const [path, search] = url.split('?');
+      if (search === 'sort=desc' || search === 'sort=asc') {
+         return (await axios.get(`https://fakestoreapi.com${url}`)).data;
+      } else {
+         const response = await axios.get(`https://fakestoreapi.com${path}`);
+         let sortBy: string = '';
+         let sortOrder: string = '';
+
+         if (search.includes('sort=lower-to-higher-price')) {
+            sortBy = 'price';
+            sortOrder = 'asc';
+         } else if (search.includes('sort=higher-to-lower-price')) {
+            sortBy = 'price';
+            sortOrder = 'desc';
+         } else if (search.includes('lower-to-higher-price')) {
+            sortBy = 'rate';
+            sortOrder = 'asc';
+         } else {
+            sortBy = 'rate';
+            sortOrder = 'desc';
+         }
+
+         const sortedProducts = sortProductsByOrder(response.data, sortBy, sortOrder);
+         return sortedProducts;
+      }
    }
-)
+);
+
+
+const sortProductsByOrder = (products: IProduct[], sortBy: string, sortOrder: string) => {
+   return products.slice().sort((a, b) => {
+      if (sortBy === 'price') {
+         if (sortOrder === 'desc') {
+            return b.price - a.price;
+         } else {
+            return a.price - b.price;
+         }
+      } else if (sortBy === 'rate') {
+         if (sortOrder === 'desc') {
+            return b.rating.rate - a.rating.rate;
+         } else {
+            return a.rating.rate - b.rating.rate;
+         }
+      }
+      return 0;
+   });
+};
+
 
 interface ProductsProps {
    filteredProducts: IProduct[]
