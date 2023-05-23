7eaf4448680a6f4eeabb1c6e84eed0555b20513b
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-23 20:13:21 +0300

    added loading skeleton for category page

--- src/pages/category/products.tsx
+++ src/pages/category/products.tsx
@@ -3,6 +3,7 @@ import { fetchProductsByUrl } from '../../store/features/filterProducts'
 import { useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 import ProductCard from './productCard'
+import LoadingSkeleton from './loading'
 
 const Products = () => {
 
@@ -11,7 +12,7 @@ const Products = () => {
    const { filteredProducts, loading, error } = useAppSelector(state => state.filterProducts)
 
    useEffect(() => {
-      if(location.search.length) {
+      if (location.search.length) {
          dispatch(fetchProductsByUrl(location.pathname + location.search))
       } else {
          dispatch(fetchProductsByUrl(location.pathname))
@@ -20,7 +21,7 @@ const Products = () => {
 
    return (
       <div className='flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
-         <ProductCard products={filteredProducts}/>
+         {!loading && filteredProducts.length ? <ProductCard products={filteredProducts} /> : <LoadingSkeleton /> }
       </div>
    )
 }
