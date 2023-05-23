818656bac41eaee345a2c00718e08bb0b0a13232
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-23 23:50:06 +0300

    bugfix

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes } from 'react'
+import { InputHTMLAttributes, useRef } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
    name: string
@@ -12,6 +12,14 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const Input: React.FC<InputProps> = ({ name, label, placeholder, wrapperStyles, inputStyles, change, ...rest }) => {
 
+   const inputRef = useRef<HTMLInputElement | null>(null);
+
+   const handleSpanClick = () => {
+      if (inputRef.current) {
+         inputRef.current.focus();
+      }
+   };
+
    return (
       <div className={'input-wrapper relative w-full ' + wrapperStyles}>
          {label && <label htmlFor={name}>{label}</label>}
@@ -20,9 +28,10 @@ const Input: React.FC<InputProps> = ({ name, label, placeholder, wrapperStyles, 
             name={name}
             className={'peer outline-none h-full text-gray-700 w-full border border-gray-500 rounded px-4 py-1 text-sm valid:border-indigo-600 ' + inputStyles}
             onChange={change}
+            ref={inputRef}
             {...rest}
          />
-         <span className='absolute text-sm text-gray-400 whitespace-nowrap left-2 translate-x-2 top-1/2 -translate-y-1/2 transition-all peer-valid:text-xs peer-valid:text-indigo-600 peer-valid:top-0 peer-valid:bg-white peer-valid:-translate-x-0'>
+         <span className='absolute text-sm text-gray-400 whitespace-nowrap left-2 translate-x-2 top-1/2 -translate-y-1/2 transition-all peer-valid:text-xs peer-valid:text-indigo-600 peer-valid:top-0 peer-valid:bg-white peer-valid:-translate-x-0' onClick={handleSpanClick}>
             {placeholder}
          </span>
       </div>
