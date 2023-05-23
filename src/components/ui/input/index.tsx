5fc8b17832ba7a7b4a2b4f2179551c7e5956fe62
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-23 21:59:28 +0300

    removed unnecessary imports

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, useState, useRef } from 'react'
+import { InputHTMLAttributes } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
    name: string
@@ -11,18 +11,16 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input: React.FC<InputProps> = ({ name, label, placeholder, wrapperStyles, inputStyles, change, ...rest }) => {
-   const inputRef = useRef<HTMLInputElement | null>(null)
 
    return (
       <div className={'input-wrapper relative w-full ' + wrapperStyles}>
          {label && <label htmlFor={name}>{label}</label>}
          <input
-            ref={inputRef}
             id={name}
             name={name}
-            {...rest}
             className={'peer outline-none h-full text-gray-700 w-full border border-gray-500 rounded px-4 py-1 text-sm valid:border-indigo-600 ' + inputStyles}
             onChange={change}
+            {...rest}
          />
          <span className='absolute text-sm text-gray-400 whitespace-nowrap left-2 translate-x-2 top-1/2 -translate-y-1/2 transition-all peer-valid:text-xs peer-valid:text-indigo-600 peer-valid:top-0 peer-valid:bg-white peer-valid:-translate-x-0'>
             {placeholder}
