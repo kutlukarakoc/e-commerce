861e3cb7fe3db384367fbd6733b51de7e2286010
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-25 21:09:45 +0300

    updated input component

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -2,15 +2,15 @@ import { InputHTMLAttributes, useRef } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
    name: string
-   label?: string
    change?: (event: React.FormEvent<HTMLInputElement>) => void
-   placeholder: string
+   placeholder?: string
    wrapperStyles?: string
    inputStyles?: string
    pattern?: string
+   inputPlaceholder?: string
 }
 
-const Input: React.FC<InputProps> = ({ name, label, placeholder, wrapperStyles, inputStyles, change, ...rest }) => {
+const Input: React.FC<InputProps> = ({ name, placeholder, inputPlaceholder, wrapperStyles, inputStyles, change, ...rest }) => {
 
    const inputRef = useRef<HTMLInputElement | null>(null);
 
@@ -22,13 +22,13 @@ const Input: React.FC<InputProps> = ({ name, label, placeholder, wrapperStyles, 
 
    return (
       <div className={'input-wrapper relative w-full ' + wrapperStyles}>
-         {label && <label htmlFor={name}>{label}</label>}
          <input
             id={name}
             name={name}
             className={'peer outline-none h-full text-gray-700 w-full border border-gray-500 rounded px-4 py-1 text-sm valid:border-indigo-600 ' + inputStyles}
             onChange={change}
             ref={inputRef}
+            placeholder={inputPlaceholder}
             {...rest}
          />
          <span className='absolute text-sm text-gray-400 whitespace-nowrap left-2 translate-x-2 top-1/2 -translate-y-1/2 transition-all peer-valid:text-xs peer-valid:text-indigo-600 peer-valid:top-0 peer-valid:bg-white peer-valid:-translate-x-0' onClick={handleSpanClick}>
