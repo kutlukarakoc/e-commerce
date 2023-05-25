0915ca7fd71ff815c51d6bd82fff0a6a9f296b2d
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-26 00:34:11 +0300

    changed title font size

--- src/pages/login-register/title.tsx
+++ src/pages/login-register/title.tsx
@@ -5,7 +5,7 @@ interface ITitle {
 const Title: React.FC<ITitle> = ({text}) => {
    return (
          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-700">
+            <h2 className="mt-10 text-center text-2xl sm:text-3xl font-semibold text-gray-700">
                {text}
             </h2>
          </div>
