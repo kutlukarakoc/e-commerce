5c0bdfc4c2986a169e5554fff7f2397aba96ecbe
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-27 18:32:57 +0300

    removed unnecessary import

--- src/pages/login-register/register.tsx
+++ src/pages/login-register/register.tsx
@@ -1,5 +1,4 @@
 import Input from '../../components/ui/input'
-import Label from '../../components/ui/label'
 import Button from '../../components/ui/button'
 import Title from './title'
 import { useState } from 'react'
