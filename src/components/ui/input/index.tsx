4edb63ba28dbe7112efe9f9c1ccc6a5ed3a5db0a
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-27 18:31:51 +0300

    updated input css

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -12,7 +12,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 const Input: React.FC<InputProps> = ({ change, type, name, placeholder, label, error, ...rest }) => {
 
    return (
-      <div className='w-full h-10'>
+      <div className='w-full'>
          {label && <label htmlFor={name} className='block text-sm font-medium leading-6 text-gray-700 mb-1.5'>{label}</label>}
          <input
             onChange={change}
@@ -20,7 +20,7 @@ const Input: React.FC<InputProps> = ({ change, type, name, placeholder, label, e
             id={name}
             name={name}
             placeholder={placeholder}
-            className={'outline-none h-full text-gray-700 w-full border border-gray-500 rounded px-4 py-1 text-sm valid:border-indigo-600 placeholder:text-gray-500 focus:border-indigo-600'}
+            className={'outline-none text-gray-700 w-full border border-gray-500 rounded px-4 py-1 text-sm valid:border-indigo-600 placeholder:text-gray-500 focus:border-indigo-600 h-10'}
             {...rest}
          />
          {error && <p className='error'>{error}</p>}
