2eec8b38ff79079862508535916ceff5d9c031be
Kutlu Karakoc <karakockutlu@gmail.com>
2023-05-29 22:11:22 +0300

    added delete and get item methods

--- src/hooks/useFirestore.ts
+++ src/hooks/useFirestore.ts
@@ -1,7 +1,6 @@
-import { doc, setDoc, updateDoc } from 'firebase/firestore'
+import { deleteDoc, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore'
 import { db } from '../firebase/index'
 import { useState } from 'react'
-
 
 export const useFirestore = () => {
    const [loading, setLoading] = useState<boolean>(false)
@@ -31,5 +30,35 @@ export const useFirestore = () => {
       }
    }
 
-   return {loading, error, setItem, updateItem}
+   const deleteItem = async (collection: string, uid: string) => {
+      setLoading(true)
+      try {
+         await deleteDoc(doc(db, collection, uid))
+         setLoading(false)
+      } catch (error) {
+         const errorMessage = 'error deleting document: ' + error
+         setError(errorMessage)
+         setLoading(false)
+      }
+   }
+
+   const getItem = async (collection: string, uid: string) => {
+      setLoading(true)
+      try {
+         const docSnap = await getDoc(doc(db, collection, uid))
+         if (docSnap.exists()) {
+            console.log('document data: ', docSnap.data())
+            return docSnap.data()
+         } else {
+            console.log('No such document!')
+         }
+         setLoading(false)
+      } catch (error) {
+         const errorMessage = 'error getting document: ' + error
+         setError(errorMessage)
+         setLoading(false)
+      }
+   }
+
+   return { loading, error, setItem, updateItem, deleteItem, getItem }
 }
