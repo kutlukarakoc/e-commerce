a0a45e5bd90a2035e791c40844d8816452c88009
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-04 01:18:23 +0300

    added wish list page skeleton

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,7 +1,27 @@
+import { TrashIcon } from '@heroicons/react/24/outline'
+import { Link } from 'react-router-dom'
+import Button from '../../components/ui/button'
+import Divider from '../../components/ui/divider'
+
 const Favorites = () => {
    return (
-      <div>
-         Favorites
+      <div className='container mx-auto my-20 flex-1'>
+         <h1 className='text-lg font-semibold'>My Wish List (3 products)</h1>
+         <Divider />
+         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+            <div className='group bg-zinc-50 h-[525px] p-8 relative rounded'>
+               <div className='absolute top-2 right-2 cursor-pointer'>
+                  <TrashIcon className='w-6 h-6' />
+               </div>
+               <Link to='/'>
+                  <img src='https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg' alt='ecommerce' className='w-full block aspect-video max-w-[250px] max-h-[275px] h-full mx-auto mix-blend-multiply' />
+               </Link>
+               <Link to='/' className='mt-10 mb-1 text-base text-left font-semibold h-6 line-clamp-1 overflow-hidden'>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</Link>
+               <p className='text-sm mb-3'>men's clothing</p>
+               <div className='font-semibold text-left mb-6'>$109.95</div>
+               <Button variant='filled' color='indigo' size='sm' className='w-full h-11'>Add to cart</Button>
+            </div>
+         </div>
       </div>
    )
 }
