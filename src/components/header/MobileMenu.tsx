a8a5f71083ccdf4fc22de43083b6f3a86e7f16bc
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-04 23:00:41 +0300

    Update MobileMenu.tsx

--- src/components/header/MobileMenu.tsx
+++ src/components/header/MobileMenu.tsx
@@ -7,10 +7,11 @@ interface CategoriesProps {
 }
 
 const MobileMenu: React.FC<{ categories: CategoriesProps[]; menuTransform: string}> = ({ categories, menuTransform }) => {
+
    return (
       <div 
          className="absolute z-10 w-64 h-72 shadow-lg bg-white top-20 left-0 flex flex-col justify-between items-start p-6 transition-transform"
-         style={{transform: menuTransform}}   
+         style={{transform: menuTransform}}
       >
          <div className="flex flex-col justify-center items-start gap-4">
             {categories.map((category: CategoriesProps, index: number) => (
