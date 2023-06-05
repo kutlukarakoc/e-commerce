85bfdbc290e37aee9e358f190228d0be6cb8d02e
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-05 22:47:13 +0300

    added variant

--- src/components/ui/divider/index.tsx
+++ src/components/ui/divider/index.tsx
@@ -1,9 +1,18 @@
 interface IDivider {
    styles?: string
+   variant?: 'soft' | 'normal'
 }
 
-const Divider: React.FC<IDivider> = ({styles}) => {
-   return <div className={'border-t border-solid border-gray-500 w-full h-1 my-5 ' + styles}></div>
+const Divider: React.FC<IDivider> = ({styles, variant}) => {
+
+   let variantColor: string
+   if(variant === 'soft') {
+      variantColor = 'border-gray-300'
+   } else {
+      variantColor = 'border-gray-500'
+   }
+
+   return <div className={`border-t border-solid w-full h-1 my-5 ${styles} ${variantColor}` }></div>
 }
 
 export default Divider
