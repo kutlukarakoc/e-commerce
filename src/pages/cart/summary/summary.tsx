e7b9e74c018cacefdbd8c3fa1e147a988f394a15
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-06 20:22:27 +0300

    moved summary to another file

---
+++ src/pages/cart/summary/summary.tsx
@@ -0,0 +1,29 @@
+import Button from '../../../components/ui/button'
+import Subtotal from './subtotal'
+import Shipping from './shipping'
+import OrderTotal from './orderTotal'
+import { useCart } from '../../../hooks/useCart'
+import { useState, useEffect } from 'react'
+
+const Summary: React.FC = () => {
+
+   // getting cart from custom hook
+   const { cart } = useCart()
+
+
+   return (
+      <div className='col-span-8 lg:col-span-3'>
+         <div className='w-full bg-zinc-100 rounded-md mt-16 sm:mt-0 px-4 py-5 sm:p-5 lg:p-8h'>
+            <h2 className='text-lg'>Order Summary</h2>
+            <div className='mt-6'>
+               <Subtotal />
+               <Shipping />
+               <OrderTotal />
+            </div>
+            <Button variant='filled' color='indigo' size='sm' className='mt-10 w-full py-4'>Checkout</Button>
+         </div>
+      </div>
+   )
+}
+
+export default Summary
