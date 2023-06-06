1dcd54a45a0c3eb4f7b9f4bc69cad22c9b8ff369
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-06 20:22:03 +0300

    splitted code and added product count

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,17 +1,18 @@
 import Products from './products'
-import Summary from './summary'
+import Summary from './summary/summary'
+import { useCart } from '../../hooks/useCart'
 
 const Cart: React.FC = () => {
+
+   // getting cart from custom hook
+   const { cart } = useCart()
+
    return (
       <section className='container mx-auto my-20 flex-1'>
-         <h1 className='text-3xl font-semibold mb-16'>Shopping Cart (5 products)</h1>
+         <h1 className='text-3xl font-semibold mb-16'>Shopping Cart ({cart.length} products)</h1>
          <div className='grid grid-cols-8'>
-            <div className='col-span-5'>
-               <Products />
-            </div>
-            <div className='col-span-3'>
-               <Summary />
-            </div>
+            <Products />
+            <Summary />
          </div>
       </section>
    )
