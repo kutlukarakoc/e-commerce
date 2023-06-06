3dab6965cf2e83a1e0f4e61806db720e79486b90
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-06 20:21:37 +0300

    added product count in cart

--- src/components/header/rightSide.tsx
+++ src/components/header/rightSide.tsx
@@ -3,6 +3,7 @@ import { UserIcon, HeartIcon, ShoppingBagIcon, ArrowRightOnRectangleIcon } from 
 import { useAppSelector, useAppDispatch } from '../../store/hooks'
 import { setToWishlist } from '../../store/features/wishlist'
 import { useWishlist } from '../../hooks/useWishlist'
+import { useCart } from '../../hooks/useCart'
 import { useEffect } from 'react'
 
 const RightSide: React.FC = () => {
@@ -15,6 +16,8 @@ const RightSide: React.FC = () => {
    const { wishlistProducts } = useAppSelector(state => state.wishlist)
    // get wishlist from custom hook
    const { wishlist } = useWishlist()
+   //get cart from custom hook
+   const { cart } = useCart()
    // set initial wishlsit
    useEffect(() => {
       dispatch(setToWishlist(wishlist))
@@ -36,7 +39,7 @@ const RightSide: React.FC = () => {
                <Link to='/cart' className='flex flex-col items-center justify-center gap-1 cursor-pointer w-8 h-8 sm:w-12 sm:h-12 relative'>
                   <ShoppingBagIcon className='w-full h-full text-gray-700' />
                   <div className='text-xs tracking-wide hidden sm:block'>Cart</div>
-                  <div className='absolute -right-1 -top-2 bg-indigo-500 rounded-full w-4 h-4 text-center text-white text-xs'>1</div>
+                  <div className='absolute -right-1 -top-2 bg-indigo-500 rounded-full w-4 h-4 text-center text-white text-xs'>{cart.length}</div>
                </Link>
             </>) : (
                <Link to='/auth' className='flex flex-col items-center justify-center gap-1 cursor-pointer'>
