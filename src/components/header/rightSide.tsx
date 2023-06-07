14213817ccf2f79471df9992f75f073424fd6487
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-07 22:43:06 +0300

    getting cart and wishlist from redux store

--- src/components/header/rightSide.tsx
+++ src/components/header/rightSide.tsx
@@ -1,26 +1,26 @@
 import { Link } from 'react-router-dom'
 import { UserIcon, HeartIcon, ShoppingBagIcon, ArrowRightOnRectangleIcon } from '@heroicons/react/24/outline'
-import { useAppSelector, useAppDispatch } from '../../store/hooks'
-import { setToWishlist } from '../../store/features/wishlist'
-import { useWishlist } from '../../hooks/useWishlist'
-import { useCart } from '../../hooks/useCart'
+import { useAppSelector } from '../../store/hooks'
 import { useEffect } from 'react'
+import { useCart } from '../../hooks/useCart'
+import { useWishlist } from '../../hooks/useWishlist'
 
 const RightSide: React.FC = () => {
-
-   const dispatch = useAppDispatch()
 
    // get user from the redux store
    const user = useAppSelector(state => state.auth.user)
    // get wishlist from redux store
-   const { wishlistProducts } = useAppSelector(state => state.wishlist)
-   // get wishlist from custom hook
-   const { wishlist } = useWishlist()
-   //get cart from custom hook
-   const { cart } = useCart()
-   // set initial wishlsit
+   const { wishlist } = useAppSelector(state => state.wishlist)
+   // getting cart from redux store
+   const { cart } = useAppSelector(state => state.cart)
+   // get wishlist and cart from custom hooks for initial value
+   const { getWishlist } = useWishlist()
+   const { getCart } = useCart()
+
+   // get initial cart and wishlist values when component mount
    useEffect(() => {
-      dispatch(setToWishlist(wishlist))
+      getWishlist()
+      getCart()
    }, [])
 
    return (
@@ -34,12 +34,16 @@ const RightSide: React.FC = () => {
                <Link to='/favorites' className='flex flex-col items-center justify-center gap-1 cursor-pointer w-8 h-8 sm:w-12 sm:h-12 relative'>
                   <HeartIcon className='w-full h-full text-gray-700' />
                   <div className='text-xs tracking-wide hidden sm:block'>Wishlist</div>
-                  <div className='absolute -right-1 -top-2 bg-indigo-500 rounded-full w-4 h-4 text-center text-white text-xs'>{wishlistProducts.length}</div>
+                  { // display wishlist count when wishlist loaded
+                     wishlist.length ? <div className='absolute -right-1 -top-2 bg-indigo-500 rounded-full w-4 h-4 text-center text-white text-xs'>{wishlist.length}</div> : null
+                  }
                </Link>
                <Link to='/cart' className='flex flex-col items-center justify-center gap-1 cursor-pointer w-8 h-8 sm:w-12 sm:h-12 relative'>
                   <ShoppingBagIcon className='w-full h-full text-gray-700' />
                   <div className='text-xs tracking-wide hidden sm:block'>Cart</div>
-                  <div className='absolute -right-1 -top-2 bg-indigo-500 rounded-full w-4 h-4 text-center text-white text-xs'>{cart.length}</div>
+                  { // display cart count when wishlist loaded
+                     cart.length ? <div className='absolute -right-1 -top-2 bg-indigo-500 rounded-full w-4 h-4 text-center text-white text-xs'>{cart.length}</div> : null
+                  }
                </Link>
             </>) : (
                <Link to='/auth' className='flex flex-col items-center justify-center gap-1 cursor-pointer'>
