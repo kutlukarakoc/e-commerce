1f4b0d9b23ac7764b16f54f714cc7c889337ac66
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-09 20:48:06 +0300

    added loading skeleton

--- src/pages/search-results/index.tsx
+++ src/pages/search-results/index.tsx
@@ -1,6 +1,7 @@
 import ProductCard from '../../components/product/card'
 import NotFound from '../../components/not-found'
-import { useEffect, useState } from 'react'
+import LoadingSkeleton from '../../components/product/loading'
+import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import { useAppSelector, useAppDispatch } from '../../store/hooks'
 import { fetchProductsBySearch } from '../../store/features/searchProducts'
@@ -13,34 +14,32 @@ const SearchResults: React.FC = () => {
    // getting searched products, loading state, and error state from the redux store
    const { searchedProducts, loading, error } = useAppSelector(state => state.searchProducts)
 
-   // manages the state of whether the search results are not found
-   const [isNotFound, setIsNotFound] = useState<boolean>(false)
-
-   // fetches products based on the search key from the URL and updates the isNotFound state.
+   // fetches products based on the search key from the URL
    useEffect(() => {
       // extracts the search key from the URL search parameters
       const key = search.split('=')[1]
-      if (key.length) {
-         dispatch(fetchProductsBySearch(key))
-         setIsNotFound(false)
-      } else {
-         setIsNotFound(true)
-      }
+      if (key.length) dispatch(fetchProductsBySearch(key))
    }, [search])
 
-   // set is not found depends on error
-   useEffect(() => {
-      error ? setIsNotFound(true) : setIsNotFound(false)
-   }, [searchedProducts, loading, error])
+   // display not found error
+   if (error) {
+      return <NotFound title='Product not found' text='Sorry, we couldn’t find the product you’re looking for.' link='/products' linkText='See products' />
+   }
+
+   // display not found product component when there is no error, false loading and no product in searched products state
+   if (!error && !loading && !searchedProducts.length) {
+      return <div className='my-20'><NotFound title='Product not found' text='Sorry, we couldn’t find the product you’re looking for.' isErrorCode={false} link='/products' linkText='See products' /></div>
+   }
 
    return (
       <div className='container mx-auto my-32'>
-         {
-            // displays error component or product card component depends on not found state
-            isNotFound
-               ? <NotFound title='Product not found' text='Sorry, we couldn’t find the product you’re looking for.' link='/' linkText='Go back home' />
-               : <div className='flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'><ProductCard products={searchedProducts} /></div>
-         }
+         <div className='flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+            { // if loading is true display loading skeleton, else display products
+               loading
+                  ? <LoadingSkeleton count={3} hasIcon={true} loading={loading} />
+                  : <ProductCard products={searchedProducts} />
+            }
+         </div>
       </div>
    )
 }
