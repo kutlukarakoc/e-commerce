276fc2244e0183ee4ce058a7b20351d528ccfec7
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-09 21:09:22 +0300

    style update

--- src/pages/home/our-picks/loading.tsx
+++ src/pages/home/our-picks/loading.tsx
@@ -19,12 +19,12 @@ const LoadingSkeleton: React.FC = () => {
          {Array.from({ length: arraySize }).map((item, index) => (
             <div key={index} className='h-64'>
                <div className='h-full w-48 sm:w-60'>
-                  <div className='bg-gray-600 w-full h-full rounded'></div>
+                  <div className='bg-gray-600 w-full h-full rounded-md'></div>
                   <div className='bg-gray-600 w-4/5 h-3 rounded-lg mt-8 mb-3'></div>
-                  <p className='bg-gray-600 w-3/12 h-2 rounded mb-3'></p>
+                  <p className='bg-gray-600 w-3/12 h-2 rounded-md mb-3'></p>
                   <div className='flex justify-between px-4 w-full'>
-                     <div className='bg-gray-600 w-11 h-2 rounded'></div>
-                     <div className='bg-gray-600 w-7 h-2 rounded'></div>
+                     <div className='bg-gray-600 w-11 h-2 rounded-md'></div>
+                     <div className='bg-gray-600 w-7 h-2 rounded-md'></div>
                   </div>
                </div>
             </div>
