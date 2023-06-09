61b59d344ecf603fd4908a9b8ac4bfc2b7affb39
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-09 21:06:45 +0300

    style update

--- src/components/product/loading.tsx
+++ src/components/product/loading.tsx
@@ -10,8 +10,8 @@ const LoadingSkeleton: React.FC<IProductLoading> = ({ count, loading, hasIcon })
          <>
             {Array.from({ length: count }).map((item, index) => (
                <div key={index} className='bg-zinc-50 h-[525px] p-8 rounded-md relative animate-pulse'>
-                  {hasIcon ? <div className='absolute top-2 right-3 w-5 h-5 rounded-full bg-gray-600'></div> : null}
-                  <div className='w-full block aspect-video max-w-[315px] max-h-[330px] h-full mx-auto bg-gray-600'></div>
+                  {hasIcon ? <div className='absolute top-2 right-3 w-5 h-6 rounded-full bg-gray-600'></div> : null}
+                  <div className='w-full block aspect-video max-w-[300px] max-h-[315px] h-full mx-auto rounded-md bg-gray-600'></div>
                   <div className='bg-gray-600 w-4/5 h-3 rounded-lg mt-8 mb-3'></div>
                   <div className='bg-gray-600 w-3/12 h-2 rounded-md mb-3'></div>
                   <div className='bg-gray-600 w-11 h-2 rounded-md mb-6'></div>
