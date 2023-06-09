bbb3910e78ce702455ef4ea755e205494283acdb
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-09 20:47:38 +0300

    added general loading skeleton for product card component

---
+++ src/components/product/loading.tsx
@@ -0,0 +1,28 @@
+interface IProductLoading {
+   count: number
+   loading: boolean
+   hasIcon: boolean
+}
+
+const LoadingSkeleton: React.FC<IProductLoading> = ({ count, loading, hasIcon }) => {
+   if (loading) {
+      return (
+         <>
+            {Array.from({ length: count }).map((item, index) => (
+               <div key={index} className='bg-zinc-50 h-[525px] p-8 rounded-md relative animate-pulse'>
+                  {hasIcon ? <div className='absolute top-2 right-3 w-5 h-5 rounded-full bg-gray-600'></div> : null}
+                  <div className='w-full block aspect-video max-w-[315px] max-h-[330px] h-full mx-auto bg-gray-600'></div>
+                  <div className='bg-gray-600 w-4/5 h-3 rounded-lg mt-8 mb-3'></div>
+                  <div className='bg-gray-600 w-3/12 h-2 rounded-md mb-3'></div>
+                  <div className='bg-gray-600 w-11 h-2 rounded-md mb-6'></div>
+                  <div className='bg-gray-600 w-full h-10 rounded-md'></div>
+               </div>
+            ))}
+         </>
+      )
+   }
+
+   return null
+}
+
+export default LoadingSkeleton
