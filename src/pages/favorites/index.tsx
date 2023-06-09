739ff7b723b651b6b424f2ea35bb6e2b780a75c8
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-09 22:40:56 +0300

    added product card component

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,4 +1,4 @@
-import WishlistProducts from './products'
+import ProductCard from '../../components/product-card'
 import Divider from '../../components/ui/divider'
 import LoadingSkeleton from '../../components/product-card/loading'
 import NotFound from '../../components/not-found'
@@ -33,7 +33,9 @@ const Favorites = () => {
       <div className='container mx-auto my-20 flex-1'>
          <h1 className='text-3xl font-semibold'>My Wishlist ({wishlist.length} products)</h1>
          <Divider variant='soft' />
-         <WishlistProducts />
+         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+            <ProductCard products={wishlist} icon='trash' />
+         </div>
       </div>
    )
 }
