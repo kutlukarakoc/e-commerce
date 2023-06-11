e922bc2b02aaa13d295d183d808c453064dad9a3
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-11 21:50:57 +0300

    Update indexDivider.cy.tsx

--- src/components/ui/divider/indexDivider.cy.tsx
+++ src/components/ui/divider/indexDivider.cy.tsx
@@ -3,21 +3,20 @@ import Divider from './index'
 
 describe('Divider Component', () => {
 
-  it('Does it have normal variant classes?', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Divider />)
-    cy.get('div').should('have.class', 'border-gray-500')
-  })
+	// variant test
+	it('Does it have normal variant classes?', () => {
+		cy.mount(<Divider />)
+		cy.get('div').should('have.class', 'border-gray-500')
+	})
+	it('Does it have soft variant classes?', () => {
+		cy.mount(<Divider variant='soft' />)
+		cy.get('div').should('have.class', 'border-gray-300')
+	})
 
-  it('Does it have soft variant classes?', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Divider variant='soft' />)
-    cy.get('div').should('have.class', 'border-gray-300')
-  })
+	// additional styles test
+	it('Does it add the styles prop as a class?', () => {
+		cy.mount(<Divider styles='my-5' />)
+		cy.get('div').should('have.class', 'my-5')
+	})
 
-  it('Does it add the styles prop as a class?', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Divider styles='my-5' />)
-    cy.get('div').should('have.class', 'my-5')
-  })
 })
