01d828dc35efcc5409a49bc8f68e064e3f95faaa
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-13 01:05:06 +0300

    style updates

--- src/pages/home/hero/index.tsx
+++ src/pages/home/hero/index.tsx
@@ -20,7 +20,7 @@ const HeroSection: React.FC = () => {
       <section className='hero-section mb-32'>
          <Slider {...heroSettings}>
             {heroBanners.map((banner, index) => (
-               <div key={index} className='h-[450px] sm:h-[625px]'>
+               <div key={index} className='h-80 sm:h-[625px]'>
                   <Link key={index} to={banner.path} className='h-full'>
                      <img src={banner.img} alt='ecommerce' className='w-full block aspect-video max-w-full h-full' />
                   </Link>
