9dbde9f880216069823652dd088a07a6d5e64b78
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-18 02:05:20 +0300

    created profile saved popup

---
+++ src/components/popup/profileSaved.tsx
@@ -0,0 +1,10 @@
+const ProfileSaved: React.FC = () => {
+
+   return (
+      <div data-cy='profile-saved' className='p-4'>
+         <h2 className='text-2xl font-medium mb-6'>Your information has been successfully saved.</h2>
+      </div>
+   )
+}
+
+export default ProfileSaved
