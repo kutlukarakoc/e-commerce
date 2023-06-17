c0218aa0427ce17630b67a827f7c6f6e4ba57f50
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-18 02:04:38 +0300

    added register form with formik yup

--- src/pages/login-register/register.tsx
+++ src/pages/login-register/register.tsx
@@ -5,40 +5,41 @@ import Spinner from '../../components/ui/spinner'
 import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline'
 import { useState } from 'react'
 import { useAuth } from '../../hooks/useAuth'
-import { useNavigate } from 'react-router-dom'
 import { useAppDispatch } from '../../store/hooks'
 import { login } from '../../store/features/auth'
+import { useNavigate } from 'react-router-dom'
 import { useFirestore } from '../../hooks/useFirestore'
 import { Formik } from 'formik'
-import { loginValidations } from '../../yup-validations/loginValidation'
+import { registerValidations } from '../../yup-validations/registerValidation'
 
-interface ILogin {
-   displayRegister: () => void
-   loginTransform: string | undefined
+interface IRegister {
+   displayLogin: () => void
 }
 
-interface ILoginValues {
-   loginEmail: string
-   loginPassword: string
+interface IRegisterValues {
+   registerName: string
+   registerSurname: string
+   registerEmail: string
+   registerPassword: string
 }
 
-const Login: React.FC<ILogin> = ({ displayRegister, loginTransform }) => {
+const Register: React.FC<IRegister> = ({ displayLogin }) => {
 
    const dispatch = useAppDispatch()
    const navigate = useNavigate()
 
    // state that specifies whether to show the password
    const [showPassword, setShowPassword] = useState<boolean>(false)
-   // getting states and signin method from useAuth custom hook
-   const { loading, error, signin } = useAuth()
+   // getting states and register method from useAuth custom hook
+   const { loading, error, register } = useAuth()
 
-   // update method from useFirestore custom hook
-   const { updateItem } = useFirestore()
+   // set method from useFirestore custom hook
+   const { setItem } = useFirestore()
 
-   // make signin request and set user state, if success navigate to home
-   const handleLogin = async (values: ILoginValues) => {
-      // signin request using custom useAuth hook
-      const response = await signin(values.loginEmail, values.loginPassword)
+   // make register request and set user state, if success navigate to home
+   const handleRegister = async (values: IRegisterValues) => {
+      // register request using custom useAuth hook
+      const response = await register(values.registerEmail, values.registerPassword)
       if (response) {
          // destructing properties from response
          const { email, emailVerified, phoneNumber, photoURL, uid, metadata } = response
@@ -48,70 +49,110 @@ const Login: React.FC<ILogin> = ({ displayRegister, loginTransform }) => {
          const payload = { email, emailVerified, phoneNumber, photoURL, uid, metadata: { creationTime, lastSignInTime } }
          // set payload to user auth.state
          dispatch(login(payload))
-         // update email verified data in firestore
-         await updateItem('users', uid, { emailVerified })
-         // navigate to home page
+         // set user to db
+         await setItem('users', uid, {
+            name: values.registerName,
+            surname: values.registerSurname,
+            email,
+            emailVerified,
+            phoneNumber,
+            photoURL,
+            uid,
+            gender: null,
+            birthday: null,
+            metadata: {
+               creationTime,
+               lastSignInTime
+            }
+         })
+         await setItem('wishlist', uid, { products: [] })
+         await setItem('cart', uid, { items: [] })
+         // navigate to profile page
          navigate('/profile')
       }
    }
 
    return (
-      <div className={'bg-white flex w-full min-h-full flex-1 flex-col justify-center z-10 absolute lg:relative transition-transform duration-300 ' + loginTransform}>
-         <Title text='Sign in to your account' />
-         <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-            <Formik
-               initialValues={{ loginEmail: '', loginPassword: '' }}
-               validationSchema={loginValidations}
-               onSubmit={(values: ILoginValues) => { handleLogin(values) }}
-            >
-               {({ errors, touched, values, handleChange, handleBlur, handleSubmit }) => (
-                  <form data-cy='login-form' className='px-4 sm:px-0' onSubmit={handleSubmit}>
-                     <div className='mb-3 h-24'>
-                        <Input
-                           data-cy='login-email'
-                           name='loginEmail'
-                           label='Email Address'
-                           type='email'
-                           placeholder='example@mail.com'
-                           onChange={handleChange}
-                           onBlur={handleBlur}
-                           value={values.loginEmail}
-                           error={(touched.loginEmail && errors.loginEmail) ? errors.loginEmail : null}
-                        />
-                     </div>
-                     <div className='mb-10 h-24 relative'>
-                        <Input
-                           data-cy='login-password'
-                           name='loginPassword'
-                           label='Password'
-                           type={showPassword ? 'text' : 'password'}
-                           placeholder='******'
-                           onChange={handleChange}
-                           onBlur={handleBlur}
-                           value={values.loginPassword}
-                           error={(touched.loginPassword && errors.loginPassword) ? errors.loginPassword : null}
-                        />
-                        {showPassword
-                           ? <EyeIcon className='w-5 h-5 absolute top-[42%] right-5 cursor-pointer' onClick={() => setShowPassword(!showPassword)} />
-                           : <EyeSlashIcon className='w-5 h-5 absolute right-5 top-[42%] cursor-pointer' onClick={() => setShowPassword(!showPassword)} />}
-                     </div>
-                     <Button disabled={loading} type='submit' variant='filled' size='md' color='indigo' className='w-56 h-11 flex justify-center items-center gap-4 mx-auto disabled:opacity-75 disabled:cursor-not-allowed'>
-                        Sign in {loading && <Spinner />}
-                     </Button>
-                  </form>
-               )}
-            </Formik>
-            {/* Display firestore erros if its exists */}
-            {error && <p className='text-center mt-5 text-sm text-red-500'>{error}</p>}
-            <div className='mt-10 text-center text-sm text-gray-500'>
-               <span>Not a member?</span>
-               <div className='text-indigo-600 cursor-pointer hover:text-indigo-500 lg:cursor-default lg:hover:text-indigo-600' onClick={displayRegister}>
-                  Register &rarr;
+      <div className='border-l border-solid border-gray-300 bg-gray-100 w-full h-full'>
+         <div className='flex min-h-full flex-1 flex-col justify-center'>
+            <Title text='Register' />
+            <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+               <Formik
+                  initialValues={{ registerName: '', registerSurname: '', registerEmail: '', registerPassword: '' }}
+                  validationSchema={registerValidations}
+                  onSubmit={(values: IRegisterValues) => { handleRegister(values) }}
+               >
+                  {({ errors, touched, values, handleChange, handleBlur, handleSubmit }) => (
+                     <form className='px-4 lg:px-0' onSubmit={handleSubmit}>
+                        <div className='mb-1 h-24 grid grid-cols-2 gap-2'>
+                           <Input
+                              name='registerName'
+                              label='Name'
+                              type='text'
+                              placeholder='Michael'
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.registerName}
+                              error={(touched.registerName && errors.registerName) ? errors.registerName : null}
+                           />
+                           <Input
+                              name='registerSurname'
+                              label='Surname'
+                              type='text'
+                              placeholder='Jackson'
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.registerSurname}
+                              error={(touched.registerSurname && errors.registerSurname) ? errors.registerSurname : null}
+                           />
+                        </div>
+                        <div className='mb-1 h-24'>
+                           <Input
+                              name='registerEmail'
+                              label='Email Address'
+                              type='email'
+                              placeholder='example@mail.com'
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.registerEmail}
+                              error={(touched.registerEmail && errors.registerEmail) ? errors.registerEmail : null}
+                           />
+                        </div>
+                        <div className='mb-10 h-24 relative'>
+                           <Input
+                              name='registerPassword'
+                              label='Password'
+                              type={showPassword ? 'text' : 'password'}
+                              placeholder='******'
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.registerPassword}
+                              error={(touched.registerPassword && errors.registerPassword) ? errors.registerPassword : null}
+                           />
+                           {showPassword
+                              ? <EyeIcon className='w-5 h-5 absolute top-[42%] right-5 cursor-pointer' onClick={() => setShowPassword(!showPassword)} />
+                              : <EyeSlashIcon className='w-5 h-5 absolute top-[42%] right-5 cursor-pointer' onClick={() => setShowPassword(!showPassword)} />
+                           }
+                        </div>
+                        <Button disabled={loading} type='submit' variant='filled' size='md' color='indigo' className='w-52 h-11 flex justify-center items-center gap-4 mx-auto disabled:opacity-75 disabled:cursor-not-allowed'>
+                           Register
+                           {loading && <Spinner />}
+                        </Button>
+                     </form>
+                  )}
+               </Formik>
+               {/* Display firestore erros if its exists */}
+               {error && <p className='text-center mt-5 text-sm text-red-500'>{error}</p>}
+               <div className='mt-10 text-center text-sm text-gray-500'>
+                  <div>Already have an account?</div>
+                  <div className='text-indigo-600 cursor-pointer hover:text-indigo-500 lg:cursor-default lg:hover:text-indigo-600' onClick={displayLogin}>
+                     &larr; Sign in!
+                  </div>
                </div>
             </div>
          </div>
-      </div >
+      </div>
    )
 }
 
-export default Login
+export default Register
