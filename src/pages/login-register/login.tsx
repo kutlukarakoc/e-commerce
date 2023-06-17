87b4a79dde8ca5c128b25e12e3390e9f80e75aae
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-17 19:52:16 +0300

    changed form with formik&yup

--- src/pages/login-register/login.tsx
+++ src/pages/login-register/login.tsx
@@ -9,39 +9,36 @@ import { useNavigate } from 'react-router-dom'
 import { useAppDispatch } from '../../store/hooks'
 import { login } from '../../store/features/auth'
 import { useFirestore } from '../../hooks/useFirestore'
+import { Formik } from 'formik'
+import { loginValidations } from '../../yup-validations/loginValidation'
 
 interface ILogin {
    displayRegister: () => void
    loginTransform: string | undefined
 }
 
+interface ILoginValues {
+   loginEmail: string
+   loginPassword: string
+}
+
 const Login: React.FC<ILogin> = ({ displayRegister, loginTransform }) => {
+
+   const dispatch = useAppDispatch()
+   const navigate = useNavigate()
 
    // state that specifies whether to show the password
    const [showPassword, setShowPassword] = useState<boolean>(false)
-   // keep track of the form's data as the user enters it
-   const [form, setForm] = useState<{ loginEmail: string, loginPassword: string }>({ loginEmail: '', loginPassword: '' })
-
    // getting states and signin method from useAuth custom hook
    const { loading, error, signin } = useAuth()
+
    // update method from useFirestore custom hook
    const { updateItem } = useFirestore()
 
-   const dispatch = useAppDispatch()
-
-   const navigate = useNavigate()
-
-   // update the form state with input values
-   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const { name, value } = event.target
-      setForm({ ...form, [name]: value })
-   }
-
    // make signin request and set user state, if success navigate to home
-   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault()
+   const handleLogin = async (values: ILoginValues) => {
       // signin request using custom useAuth hook
-      const response = await signin(form.loginEmail, form.loginPassword)
+      const response = await signin(values.loginEmail, values.loginPassword)
       if (response) {
          // destructing properties from response
          const { email, emailVerified, phoneNumber, photoURL, uid, metadata } = response
@@ -61,27 +58,51 @@ const Login: React.FC<ILogin> = ({ displayRegister, loginTransform }) => {
    return (
       <div className={'bg-white flex w-full min-h-full flex-1 flex-col justify-center z-10 absolute lg:relative transition-transform duration-300 ' + loginTransform}>
          <Title text='Sign in to your account' />
-
          <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-            <form data-cy='login-form' className='px-8 sm:px-0' onSubmit={handleSubmit}>
-               <div className='mb-6'>
-                  <Input data-cy='login-email' name='loginEmail' label='Email Address' type='email' placeholder='example@mail.com' onChange={handleInputChange} required />
-               </div>
-               <div className='mb-10 relative'>
-                  <Input data-cy='login-password' name='loginPassword' label='Password' type={showPassword ? 'text' : 'password'} placeholder='******' onChange={handleInputChange} required />
-                  {showPassword
-                     ? <EyeIcon className='w-5 h-5 absolute top-[55%] right-5 cursor-pointer' onClick={() => setShowPassword(!showPassword)} />
-                     : <EyeSlashIcon className='w-5 h-5 absolute top-[55%] right-5 cursor-pointer' onClick={() => setShowPassword(!showPassword)} />}
-               </div>
-
-               <Button disabled={loading} type='submit' variant='filled' size='md' color='indigo' className='w-56 h-11 flex justify-center items-center gap-4 mx-auto disabled:opacity-75 disabled:cursor-not-allowed'>
-                  Sign in
-                  {loading && <Spinner />}
-               </Button>
-            </form>
-
+            <Formik
+               initialValues={{ loginEmail: '', loginPassword: '' }}
+               validationSchema={loginValidations}
+               onSubmit={(values: ILoginValues) => { handleLogin(values) }}
+            >
+               {({ errors, touched, values, handleChange, handleBlur, handleSubmit }) => (
+                  <form data-cy='login-form' className='px-8 sm:px-0' onSubmit={handleSubmit}>
+                     <div className='mb-3 h-24'>
+                        <Input
+                           data-cy='login-email'
+                           name='loginEmail'
+                           label='Email Address'
+                           type='email'
+                           placeholder='example@mail.com'
+                           onChange={handleChange}
+                           onBlur={handleBlur}
+                           value={values.loginEmail}
+                           error={(touched.loginEmail && errors.loginEmail) ? errors.loginEmail : null}
+                        />
+                     </div>
+                     <div className='mb-10 h-24 relative'>
+                        <Input
+                           data-cy='login-password'
+                           name='loginPassword'
+                           label='Password'
+                           type={showPassword ? 'text' : 'password'}
+                           placeholder='******'
+                           onChange={handleChange}
+                           onBlur={handleBlur}
+                           value={values.loginPassword}
+                           error={(touched.loginPassword && errors.loginPassword) ? errors.loginPassword : null}
+                        />
+                        {showPassword
+                           ? <EyeIcon className='w-5 h-5 absolute top-[42%] right-5 cursor-pointer' onClick={() => setShowPassword(!showPassword)} />
+                           : <EyeSlashIcon className='w-5 h-5 absolute right-5 top-[42%] cursor-pointer' onClick={() => setShowPassword(!showPassword)} />}
+                     </div>
+                     <Button disabled={loading} type='submit' variant='filled' size='md' color='indigo' className='w-56 h-11 flex justify-center items-center gap-4 mx-auto disabled:opacity-75 disabled:cursor-not-allowed'>
+                        Sign in {loading && <Spinner />}
+                     </Button>
+                  </form>
+               )}
+            </Formik>
+            {/* Display firestore erros if its exists */}
             {error && <p className='text-center mt-5 text-sm text-red-500'>{error}</p>}
-
             <div className='mt-10 text-center text-sm text-gray-500'>
                <span>Not a member?</span>
                <div className='text-indigo-600 cursor-pointer hover:text-indigo-500 lg:cursor-default lg:hover:text-indigo-600' onClick={displayRegister}>
@@ -89,7 +110,7 @@ const Login: React.FC<ILogin> = ({ displayRegister, loginTransform }) => {
                </div>
             </div>
          </div>
-      </div>
+      </div >
    )
 }
 
