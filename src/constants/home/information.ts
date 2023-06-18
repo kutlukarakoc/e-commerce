bbee282e9aef6dc86fba0fa5e9e82fbcefea4552
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-18 14:29:40 +0300

    make component more dynamic

---
+++ src/constants/home/information.ts
@@ -0,0 +1,20 @@
+import { TruckIcon, ShieldCheckIcon, BanknotesIcon, DevicePhoneMobileIcon } from '@heroicons/react/24/outline'
+
+export const informations = [
+   {
+      icon: TruckIcon,
+      title: 'Free Shipping'
+   },
+   {
+      icon: ShieldCheckIcon,
+      title: 'Secure Shopping'
+   },
+   {
+      icon: BanknotesIcon,
+      title: 'Easy Returns/Exchanges'
+   },
+   {
+      icon: DevicePhoneMobileIcon,
+      title: 'Mobile App'
+   }
+]
--- src/pages/home/information/index.tsx
+++ src/pages/home/information/index.tsx
@@ -1,24 +1,17 @@
-import { TruckIcon, ShieldCheckIcon, BanknotesIcon, DevicePhoneMobileIcon } from '@heroicons/react/24/outline'
+import { informations } from '../../../constants/home/information'
 
 const Information: React.FC = () => {
    return (
       <section className='container mx-auto grid grid-cols-2 md:grid-cols-4 mb-32'>
-         <div className='md:border-r-2 md:border-solid md:border-gray-500 text-center py-8 md:py-3 md:pe-10'>
-            <TruckIcon className='h-16 sm:h-20 w-16 sm:w-20 mx-auto text-gray-700' />
-            <h4 className='mt-4 flex justify-center'>Free Shipping</h4>
-         </div>
-         <div className='md:border-r-2 md:border-solid md:border-gray-500 text-center py-8 md:py-3 md:px-10'>
-            <ShieldCheckIcon className='h-16 sm:h-20 w-16 sm:w-20 mx-auto text-gray-700' />
-            <h4 className='mt-4 flex justify-center'>Secure Shopping</h4>
-         </div>
-         <div className='md:border-r-2 md:border-solid md:border-gray-500 text-center py-8 md:py-3 md:px-10'>
-            <BanknotesIcon className='h-16 sm:h-20 w-16 sm:w-20 mx-auto text-gray-700' />
-            <h4 className='mt-4 flex justify-center'>Easy Returns/Exchanges</h4>
-         </div>
-         <div className='text-center py-8 md:py-3 md:ps-10'>
-            <DevicePhoneMobileIcon className='h-16 sm:h-20 w-16 sm:w-20 mx-auto text-gray-700' />
-            <h4 className='mt-4 flex justify-center'>Mobile App</h4>
-         </div>
+         {informations.map((item, index) => {
+            const IconComponent = item.icon
+            return (
+               <div key={index} className={`text-center ${index < 2 ? 'mb-6 md:mb-0' : ''}`}>
+                  <IconComponent className='h-16 sm:h-20 w-16 sm:w-20 mx-auto text-gray-700' />
+                  <h4 className='mt-3 md:mt-4 text-center'>{item.title}</h4>
+               </div>
+            )
+         })}
       </section>
    )
 }
