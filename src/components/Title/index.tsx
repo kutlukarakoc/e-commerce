677613ba40ec1c53290085d52f87a355c570b46b
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-19 02:15:01 +0300

    Update index.tsx

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -9,10 +9,7 @@ const Title: React.FC<ITitle> = ({ cypressAttr, children, className }) => {
    const initialTitleClasses = 'mt-10 text-center text-2xl sm:text-3xl font-semibold text-gray-700'
 
    return (
-      <h2
-         data-cy={cypressAttr ? cypressAttr : null}
-         className={`${initialTitleClasses} ${className}`}
-      >
+      <h2 data-cy={cypressAttr ? cypressAttr : null} className={`${initialTitleClasses} ${className}`}>
          {children}
       </h2>
    )
