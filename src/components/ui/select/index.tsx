23cb30290b74acffafbcd95140245bdf8d2df21d
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-18 16:55:45 +0300

    added close when click outside feature

--- src/components/ui/select/index.tsx
+++ src/components/ui/select/index.tsx
@@ -1,18 +1,36 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { ChevronDownIcon } from '@heroicons/react/24/outline'
 import { ISelect, ISelectList } from '../../../types/selectTypes'
 
 const Select: React.FC<ISelect> = ({ title, contents, click, initialTitle }) => {
 
+   // keep track of selected option
    const [selected, setSelected] = useState<ISelectList>({ title: title })
+   // keep track of whether the show contents or not
    const [open, setOpen] = useState<boolean>(false)
+   // ref of container element of component
+   const selectRef = useRef<HTMLDivElement>(null)
 
-   const handleClick = (content: ISelectList) => {
+   // set selected option, close contents, execute additional function that given as props if exists
+   const handleOptionClick = (content: ISelectList) => {
       setSelected(content)
       setOpen(false)
 
-      if (click) click(content)
+      click && click(content)
    }
+
+   // close contents when click outside of component
+   useEffect(() => {
+      const handleClickOutside = (event: MouseEvent) => {
+         if (selectRef.current && !selectRef.current.contains(event.target as Node)) {
+            setOpen(false)
+         }
+      }
+
+      document.addEventListener('mousedown', handleClickOutside)
+      // remove event when component unmount
+      return () => document.removeEventListener('mousedown', handleClickOutside)
+   }, [])
 
    // initial select title based on url
    useEffect(() => {
@@ -20,7 +38,7 @@ const Select: React.FC<ISelect> = ({ title, contents, click, initialTitle }) => 
    }, [initialTitle])
 
    return (
-      <div data-cy='container' className={`w-48 font-medium text-sm relative cursor-pointer ${!open ? 'h-15' : ''}`}>
+      <div data-cy='container' ref={selectRef} className={`w-48 font-medium text-sm relative cursor-pointer ${!open ? 'h-15' : ''}`}>
          {/* Select header */}
          <div
             data-cy='toggle'
@@ -33,7 +51,7 @@ const Select: React.FC<ISelect> = ({ title, contents, click, initialTitle }) => 
          <ul data-cy='content-wrapper' className={`bg-white mt-2 rounded-md overflow-y-auto w-48 absolute z-10 shadow-md ${open ? 'max-h-40' : 'max-h-0'} `}>
             {selected && selected.title !== title && (
                // Default select option
-               <li className='p-2 text-xs hover:bg-indigo-600 hover:text-white' onClick={() => handleClick({ title })}>
+               <li className='p-2 text-xs hover:bg-indigo-600 hover:text-white' onClick={() => handleOptionClick({ title })}>
                   {title}
                </li>
             )}
@@ -43,7 +61,7 @@ const Select: React.FC<ISelect> = ({ title, contents, click, initialTitle }) => 
                <li key={index}
                   data-cy='content'
                   className={`p-2 text-xs hover:bg-indigo-600 hover:text-white ${content.title === selected.title && 'bg-indigo-600 text-white'}`}
-                  onClick={() => handleClick(content)}
+                  onClick={() => handleOptionClick(content)}
                >
                   {content?.title}
                </li>
