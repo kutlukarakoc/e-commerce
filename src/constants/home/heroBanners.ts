d3c4d244257c3c8bd4201a028fa39a9985d39685
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-19 21:22:42 +0300

    changed member banner

--- src/constants/home/heroBanners.ts
+++ src/constants/home/heroBanners.ts
@@ -1,11 +1,11 @@
-import memberİmg from '../../assets/hero-section/member-img.png'
 import saleİmg from '../../assets/hero-section/sale-img.webp'
 import shippingİmg from '../../assets/hero-section/shipping-img.jpeg'
+import shopBanner from '../../assets/hero-section/shop-banner.jpg'
 
 export const heroBanners = [
    {
       path: '/',
-      img: memberİmg
+      img: shopBanner
    },
    {
       path: '/',
